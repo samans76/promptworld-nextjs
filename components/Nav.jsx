86c1feea866f980f1cd14eb3d84722b18a14b581
Saman <samans767676@gmail.com>
2023-05-29 17:24:37 +0330

    init navbar

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -1,14 +1,27 @@
-import Link from "next/link"
-import Image from "next/image"
-import { useState, useEffect } from "react"
-import { signIn, signOut, useSession, getProviders } from "/next-auth/react"
+"use client";
+
+import Link from "next/link";
+import Image from "next/image";
+import { useState, useEffect } from "react";
+// import { signIn, signOut, useSession, getProviders } from "/next-auth/react"
 
 const Nav = () => {
   return (
-    <div className="w-full flex-row justify-between">
-<img src="/assets/images/logo" alt="logo" />
+    <div className="w-full flex flex-row justify-between">
+      <div className="websiteInfo flex flex-row-reverse text-center">
+        <Link className="text-black m-2" href="/">Prompt World</Link>
+        <Image
+          src={"/assets/images/logo.svg"}
+          alt="logo"
+          width={35}
+          height={35}
+        />
+      </div>
+      <div className="websiteInfo flex flex-row-reverse text-center">
+<Link href="/profile"></Link>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Nav
+export default Nav;
